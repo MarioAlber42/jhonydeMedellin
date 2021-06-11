5f49fb8a340f14c15c5cb0c667098f728764eff5
MarioAlber42 <84862759+MarioAlber42@users.noreply.github.com>
2021-06-11 17:11:12 -0500

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,7 @@ let img = document.querySelector('#modal__img');
 let boton = document.querySelector('#modal__boton');
 
 for (let i = 0; i < imagenes.length; i++){
-   imagenes[i].addEventListener('click', function(){
+   imagenes[i].addEventListener('click', function(e){
        modal.classList.toggle("modal--open");
        let src = e.target.src;
        img.setAttribute("src", src);
